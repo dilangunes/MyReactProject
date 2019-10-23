97b8ab973c8b34eb876341f79a22e07c4d3858d7
dilangunes <dilangunes91@gmail.com>
2019-10-23 12:01:43 +0300

    ScrollView_2

--- HelloWorldMyProject/App.js
+++ HelloWorldMyProject/App.js
@@ -4,25 +4,29 @@ import {
    View,
    Button,
    Text,
-   ScrollView
+   ScrollView,
+   Dimensions
 } from 'react-native';
-
+const { width } = Dimensions.get('window') //ekranın genişliğini tutar
 export default class App extends Component {
     render() {
     return (
       <View style={styles.container}>
-        <ScrollView>
+        <ScrollView
+         horizontal= {true} //yatay kaydırma yapar
+         pagingEnabled = {true} //kaydırma gecişlerini sayfalama gibi yapar
+         >
           <Text style= {styles.title}> 1 </Text>
-          <Text style= {styles.title}> 1 </Text>
-          <Text style= {styles.title}> 1 </Text>
-          <Text style= {styles.title}> 1 </Text>
-          <Text style= {styles.title}> 1 </Text>
-          <Text style= {styles.title}> 1 </Text>
-          <Text style= {styles.title}> 1 </Text>
-          <Text style= {styles.title}> 1 </Text>
-          <Text style= {styles.title}> 1 </Text>
-          <Text style= {styles.title}> 1 </Text>
-          <Text style= {styles.title}> 1 </Text>
+          <Text style= {styles.title}> 2 </Text>
+          <Text style= {styles.title}> 3 </Text>
+          <Text style= {styles.title}> 4 </Text>
+          <Text style= {styles.title}> 5 </Text>
+          <Text style= {styles.title}> 6 </Text>
+          <Text style= {styles.title}> 7 </Text>
+          <Text style= {styles.title}> 8 </Text>
+          <Text style= {styles.title}> 9 </Text>
+          <Text style= {styles.title}> 10 </Text>
+          <Text style= {styles.title}> 11 </Text>
         </ScrollView>     
       </View>       
     );
@@ -35,8 +39,8 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'center',
-    alignItems: 'center',
-    paddingHorizontal: 10,
+    //alignItems: 'center',
+    //paddingHorizontal: 10,
 
   },
  title:{
@@ -45,6 +49,8 @@ const styles = StyleSheet.create({
    marginVertical: 30,
    textAlign: 'center',
    paddingVertical: 60,
+   width: width,
+   fontSize : 40
  }
 });
 
